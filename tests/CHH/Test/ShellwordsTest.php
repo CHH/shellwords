0129a0d4a111ee9e76f3f093a03007eefa037717
Christoph Hochstrasser <christoph.hochstrasser@gmail.com>
2012-05-15 21:23:50 +0200

    Initial Commit.

---
+++ tests/CHH/Test/ShellwordsTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace CHH\Test;
+
+use CHH\Shellwords;
+
+class ShellwordsTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider splitDataProvider
+     */
+    function testSplit($input, $expected)
+    {
+        $actual = Shellwords::split($input);
+        $this->assertEquals($expected, $actual);
+    }
+
+    function splitDataProvider()
+    {
+        return array(
+            array('foo "bar baz"', array('foo', 'bar baz')),
+            array('foo \n bar', array("foo", '\n', "bar")),
+            array('\'foo bar\' baz', array("foo bar", "baz")),
+            array('"foo', array())
+        );
+    }
+}
+
